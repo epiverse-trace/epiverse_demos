be5d0fc71cb6a0fe2a18a3b78c850809b6a8528b
Andree Valle Campos <avallecam@gmail.com>
2025-02-12 14:21:40 +0000

    edits to transmission clusters and superspreading

--- transmission_var.R
+++ transmission_var.R
@@ -15,9 +15,10 @@ set.seed(1)
 
 # Simulate outbreak -------------------------------------------------------
 
-## Contact distribution R = 1.2 and k = 0.5 (i.e. overdispersed contact and transmission).
+## Contact distribution R = 1.2 and k = 0.5
+## (i.e. overdispersed contact and transmission).
 
-outbreak <- sim_outbreak(
+outbreak <- simulist::sim_outbreak(
   contact_distribution = function(x) dnbinom(x = x, mu = 1.2, size = 0.4),
   prob_infection = 0.5,
   outbreak_size = c(50, 100)
@@ -25,7 +26,7 @@ outbreak <- sim_outbreak(
 
 # Plot contact network ----------------------------------------------------
 
-contact_net <- make_epicontacts(
+contact_net <- epicontacts::make_epicontacts(
   linelist = outbreak$linelist,
   contacts = outbreak$contacts,
   id = "case_name",
@@ -41,7 +42,7 @@ plot(contact_net)
 
 transmission_net <- outbreak$contacts[outbreak$contacts$was_case == "Y", ]
 
-transmission_net <- make_epicontacts(
+transmission_net <- epicontacts::make_epicontacts(
   linelist = outbreak$linelist,
   contacts = transmission_net,
   id = "case_name",
@@ -76,22 +77,22 @@ all_cases <- sort(c(rep(0, num_no_transmit), secondary_cases))
 # - Negative Binomial
 # - Poisson-lognormal compound
 # - Poisson-Weibull compound
-pois_fit <- fitdist(data = all_cases, distr = "pois")
-geom_fit <- fitdist(data = all_cases, distr = "geom")
-nbinom_fit <- fitdist(data = all_cases, distr = "nbinom")
-poislnorm_fit <- fitdist(
+pois_fit <- fitdistrplus::fitdist(data = all_cases, distr = "pois")
+geom_fit <- fitdistrplus::fitdist(data = all_cases, distr = "geom")
+nbinom_fit <- fitdistrplus::fitdist(data = all_cases, distr = "nbinom")
+poislnorm_fit <- fitdistrplus::fitdist(
   data = all_cases,
   distr = "poislnorm",
   start = list(meanlog = 1, sdlog = 1)
 )
-poisweibull_fit <- fitdist(
+poisweibull_fit <- fitdistrplus::fitdist(
   data = all_cases,
   distr = "poisweibull",
   start = list(shape = 1, scale = 1)
 )
 
 # compare model fits
-model_tbl <- ic_tbl(
+model_tbl <- superspreading::ic_tbl(
   pois_fit, geom_fit, nbinom_fit, poislnorm_fit, poisweibull_fit
 )
 model_tbl
@@ -106,24 +107,49 @@ message("R = ", signif(R, digits = 4), "\n", "k = ", signif(k, digits = 4))
 
 # Estimate proportions of cases occur in clusters of >= a given si --------
 
-proportion_cluster_size(R = R, k = k, cluster_size = c(2, 5, 10))
+superspreading::proportion_cluster_size(
+  R = R, 
+  k = k, 
+  cluster_size = c(2, 5, 10)
+)
 
 
 # Estimate proportion of cases causing 80% transmission -------------------
 
-proportion_transmission(R = R, k = k, percent_transmission = 0.8)
+superspreading::proportion_transmission(
+  R = R, 
+  k = k, 
+  percent_transmission = 0.8
+)
 
 # Estimate probability of outbreak extinction -----------------------------
 
-probability_extinct(R = R, k = k, num_init_infect = 1)
+superspreading::probability_extinct(
+  R = R,
+  k = k,
+  num_init_infect = 1
+)
 
 # calculate upper confidence interval for R estimate to explore extinction probability
 R_upper_bound <- nbinom_fit$estimate[["mu"]] + (qnorm(0.975) * nbinom_fit$sd[["mu"]])
 
-probability_extinct(R = R_upper_bound, k = k, num_init_infect = 1)
+superspreading::probability_extinct(
+  R = R_upper_bound, 
+  k = k, 
+  num_init_infect = 1
+)
 
 # increase number of initial introductions seeding outbreaks to assess risk
-probability_extinct(R = R_upper_bound, k = k, num_init_infect = 10)
+superspreading::probability_extinct(
+  R = R_upper_bound,
+  k = k,
+  num_init_infect = 10
+)
 
 # apply small control measure on transmission to see affect on extinction probability
-probability_extinct(R = R, k = k, num_init_infect = 10, ind_control = 0.1)
+superspreading::probability_extinct(
+  R = R,
+  k = k,
+  num_init_infect = 10,
+  ind_control = 0.1
+)
